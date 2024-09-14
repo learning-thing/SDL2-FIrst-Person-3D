0d1d063951f76e1f28883f0ec8462a8e1a60993a
Tinolm <64424228+learning-thing@users.noreply.github.com>
2024-09-14 19:00:15 +0200

    my changes
    movement and input rework

--- cube_1stperson.cpp
+++ cube_1stperson.cpp
@@ -10,38 +10,48 @@
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
-
-SDL_Window* gWindow = NULL;
+SDL_Window *gWindow = NULL;
 SDL_GLContext gContext;
 
+float camup = 0;
 float cameraX = 0.0f;
 float cameraY = 0.0f;
 float cameraZ = -5.0f;
-float yaw = 0.0f;
 
+float xSpeed = 0.0f;
+float zSpeed = 0.0f;
+
+float acceleration = .03f;
+float maxSpeed = 1.0f;
+
+float yaw = 0.0f;
+const unsigned char *keys;
 
 void initSDL()
 {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
     gWindow = SDL_CreateWindow("QCMs First Person Demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
     gContext = SDL_GL_CreateContext(gWindow);
+    keys = SDL_GetKeyboardState(NULL);
+    SDL_SetRelativeMouseMode(SDL_TRUE);
+
     SDL_GL_SetSwapInterval(1);
 
-    //glEnable(GL_DEPTH_TEST); // Enable depth testing
-    //glDepthFunc(GL_LESS);    // Use less-than depth test function
-    //glEnable(GL_CULL_FACE);
+    // glEnable(GL_DEPTH_TEST); // Enable depth testing
+    // glDepthFunc(GL_LESS);    // Use less-than depth test function
+    // glEnable(GL_CULL_FACE);
 }
 
 void closeSDL()
 {
+    SDL_SetRelativeMouseMode(SDL_TRUE);
     SDL_GL_DeleteContext(gContext);
     SDL_DestroyWindow(gWindow);
     SDL_Quit();
 }
-
-
 
 void renderCube()
 {
@@ -134,11 +144,9 @@ void renderCube()
     glPopMatrix();
 }
 
-
-
 void renderPlane()
 {
-    //glDisable(GL_CULL_FACE); //disable for the plane
+    // glDisable(GL_CULL_FACE); //disable for the plane
     glPushMatrix();
     glTranslatef(0.0f, -1.0f, 0.0f);
     glColor3f(0.0f, 1.0f, 0.0f); // Green color
@@ -164,19 +172,20 @@ void renderPlane()
     glEnd();
 
     glPopMatrix();
-    //glEnable(GL_CULL_FACE); //re enable after drawing plane
+    // glEnable(GL_CULL_FACE); //re enable after drawing plane
 }
-
 
 void render()
 {
+    //SDL_Delay(16);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(45.0, (double)SCREEN_WIDTH / (double)SCREEN_HEIGHT, 0.1, 100.0);
+    SDL_SetHint(SDL_HINT_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR, "0");
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    gluLookAt(cameraX, cameraY, cameraZ, cameraX + sin(yaw), cameraY, cameraZ + cos(yaw), 0.0f, 1.0f, 0.0f); //for mouse
+    gluLookAt(cameraX, cameraY, cameraZ, cameraX + sin(yaw), cameraY+sin(camup), cameraZ + cos(yaw), 0.0f, 1.0f, 0.0f); // for mouse
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     renderPlane();
@@ -185,65 +194,106 @@ void render()
     SDL_GL_SwapWindow(gWindow);
 }
 
-void handleKeyboardEvent(const SDL_Event& event)
+void handleKeyboardEventOld(const SDL_Event &event)
 {
     switch (event.key.keysym.sym)
     {
-        case SDLK_w:
-            if (SDL_GetModState() & KMOD_SHIFT)
-            {
-                cameraX += 0.1f * sin(yaw);
-                cameraZ += 0.1f * cos(yaw);
-            }
-            else
-            {
-                cameraX += 0.1f * sin(yaw);
-                cameraZ += 0.1f * cos(yaw);
-            }
-            break;
-        case SDLK_s:
-            if (SDL_GetModState() & KMOD_SHIFT)
-            {
-                cameraX -= 0.1f * sin(yaw);
-                cameraZ -= 0.1f * cos(yaw);
-            }
-            else
-            {
-                cameraX -= 0.1f * sin(yaw);
-                cameraZ -= 0.1f * cos(yaw);
-            }
-            break;
-        case SDLK_a:
-            if (SDL_GetModState() & KMOD_SHIFT)
-                yaw += 0.5f;
-            else
-                yaw += 0.1f;
-            break;
-        case SDLK_d:
-            if (SDL_GetModState() & KMOD_SHIFT)
-                yaw -= 0.5f;
-            else
-                yaw -= 0.1f;
-            break;
+    case SDLK_w:
+        if (SDL_GetModState() & KMOD_SHIFT)
+        {
+            cameraX += 0.1f * sin(yaw);
+            cameraZ += 0.1f * cos(yaw);
+        }
+        else
+        {
+            cameraX += 0.1f * sin(yaw);
+            cameraZ += 0.1f * cos(yaw);
+        }
+        break;
+    case SDLK_s:
+        if (SDL_GetModState() & KMOD_SHIFT)
+        {
+            cameraX -= 0.1f * sin(yaw);
+            cameraZ -= 0.1f * cos(yaw);
+        }
+        else
+        {
+            cameraX -= 0.1f * sin(yaw);
+            cameraZ -= 0.1f * cos(yaw);
+        }
+        break;
+    case SDLK_a:
+        if (SDL_GetModState() & KMOD_SHIFT)
+            yaw += 0.5f;
+        else
+            yaw += 0.1f;
+        break;
+    case SDLK_d:
+        if (SDL_GetModState() & KMOD_SHIFT)
+            yaw -= 0.5f;
+        else
+            yaw -= 0.1f;
+        break;
     }
 }
 
-float getCameraYaw(SDL_Event& event)
-{
-    static int prevMouseX = 0;
+void physics() {
+    cameraX += xSpeed;
+    cameraZ += zSpeed;
+
+    xSpeed *= .9f;
+    zSpeed *= .9f;
+}
+
+bool handleKeyboardEvent(const SDL_Event &event) {
+    SDL_PumpEvents();
+    if (keys[SDL_SCANCODE_W]) {
+        xSpeed += acceleration * sin(yaw);
+        zSpeed += acceleration * cos(yaw);
+    }
+    if (keys[SDL_SCANCODE_S]) {
+        xSpeed -= acceleration * sin(yaw);
+        zSpeed -= acceleration * cos(yaw);
+    }
+    if (keys[SDL_SCANCODE_A]) {
+        xSpeed += acceleration * cos(-yaw);
+        zSpeed += acceleration * sin(-yaw);
+    }
+    if (keys[SDL_SCANCODE_D]) {
+        xSpeed -= acceleration * cos(-yaw);
+        zSpeed -= acceleration * sin(-yaw);
+    }
+    if (keys[SDL_SCANCODE_ESCAPE]) {
+        return true;
+    }
+
+    if (xSpeed > maxSpeed) {
+        xSpeed = maxSpeed;
+    }
+    if (zSpeed > maxSpeed) {
+        zSpeed = maxSpeed;
+    }
+    if (SDL_QuitRequested()) {
+        return true;
+    }
+    return false;//return value determines quitting
+}
+
+
+float getCameraYaw(SDL_Event &event) {
     float sensitivity = 0.005f;
 
-    if (event.type == SDL_MOUSEMOTION && event.motion.state & SDL_BUTTON(SDL_BUTTON_LEFT))
+    if (event.type == SDL_MOUSEMOTION)
     {
-        int deltaX = event.motion.x - prevMouseX;
-        yaw += deltaX * sensitivity;
-        prevMouseX = event.motion.x;
+        int deltaX = event.motion.xrel;
+        yaw -= deltaX * sensitivity;
+        //std::clog << event.motion.xrel << "\n";
     }
 
     return yaw;
 }
 
-void updateFaceCulling(const glm::vec3& objectPosition, float cameraX, float cameraY, float cameraZ, float yaw)
+void updateFaceCulling(const glm::vec3 &objectPosition, float cameraX, float cameraY, float cameraZ, float yaw)
 {
     // Calculate the camera position in world space
     glm::vec3 cameraPosition(cameraX, cameraY, cameraZ);
@@ -257,8 +307,8 @@ void updateFaceCulling(const glm::vec3& objectPosition, float cameraX, float cam
     // Calculate the dot product of the camera-to-object vector and the camera's forward vector
     float dotProduct = glm::dot(cameraToObject, glm::vec3(sin(angle), 0.0f, cos(angle)));
 
-    // Invert the dot product by multiplying it with -1.0
-    dotProduct *= -1.0f;
+    // Invert the dot product 
+    dotProduct = -dotProduct;
 
     // Determine the face culling mode based on the inverted dot product
     if (dotProduct > 0.0f)
@@ -270,14 +320,11 @@ void updateFaceCulling(const glm::vec3& objectPosition, float cameraX, float cam
     else
     {
         // Object is in front of the camera, enable front face culling
-        glEnable(GL_CULL_FACE);
-        glCullFace(GL_FRONT);
+        // glCullFace(GL_FRONT);
     }
 }
 
-
-
-int main(int argc, char* args[])
+int main(int argc, char *args[])
 {
     initSDL();
 
@@ -286,25 +333,20 @@ int main(int argc, char* args[])
 
     while (!quit)
     {
-        while (SDL_PollEvent(&event) != 0) // Use `event` instead of `e`
-        {
-            if (event.type == SDL_QUIT)
-                quit = true;
-            else if (event.type == SDL_KEYDOWN)
-                handleKeyboardEvent(event);
-        }
-        
+        quit = handleKeyboardEvent(event);
+        physics();
+
         // Get the center coordinates of the window
         int centerX = SCREEN_WIDTH / 2;
         int centerY = SCREEN_HEIGHT / 2;
 
         // Get the yaw angle from your camera
-        float yaw = getCameraYaw(event);
-
+        while (SDL_PollEvent(&event)) {
+            float yaw = getCameraYaw(event);
+        }
         glm::vec3 objectPosition(0.0f, 0.0f, 0.0f); // Replace with the actual object position
         //updateFaceCulling(objectPosition, cameraX, cameraY, cameraZ, yaw);
         render();
-    
     }
 
     closeSDL();
